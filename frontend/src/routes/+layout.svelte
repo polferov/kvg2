35205e9b42a1d5455059061d235408148b0d2f71
Kirill Polferov <kirill.polferov@appfact.de>
2023-02-08 00:57:47 +0100

    add +layout.svelte

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+    onMount(()=> {
+        console.log(document.body)
+    })
+</script>
+
+<slot/>
+
+<style>
+    :global(html, body) {
+        padding: 0;
+        margin: 0;
+        border: none;
+    }
+</style>
