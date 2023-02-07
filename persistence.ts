2cca08d046e0ab6ac1f9e2d975053eba6275faa1
Kirill Polferov <kirill.polferov@appfact.de>
2023-02-07 21:41:02 +0100

    create temporary persisnence impl (in memory)

---
+++ persistence.ts
@@ -0,0 +1,19 @@
+import { Stop } from "./kvg-stops.ts";
+
+const stops : Stop[] = []
+
+// deno-lint-ignore require-await
+export async function addStop(stop: Stop): Promise<Stop[]> {
+    const index = stops.findIndex(s => s.id === stop.id)
+    if(index >= 0){
+        stops[index].name = stop.name
+        return stops
+    }
+    stops.push(stop)
+    return stops
+}
+
+// deno-lint-ignore require-await
+export async function getStops(): Promise<Stop[]>{
+    return stops
+}
