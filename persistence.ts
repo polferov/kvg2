4dbab91228ed69776e741c2fc6257c56490c1be2
Kirill Polferov <kirill.polferov@appfact.de>
2023-02-08 21:06:03 +0100

    persist stops

--- persistence.ts
+++ persistence.ts
@@ -1,6 +1,7 @@
 import { Stop } from "./types.ts";
+import { path } from './deps.ts'
 
-const stops : Stop[] = []
+let stops: Stop[] = []
 
 export enum AddStopEffect {
     Added,
@@ -8,21 +9,42 @@ export enum AddStopEffect {
     None
 }
 
-// deno-lint-ignore require-await
+const stopsFileName = `${path.dirname(path.fromFileUrl(import.meta.url))}/stops.json`
+
 export async function addStop(stop: Stop): Promise<AddStopEffect> {
+    await restoreStops()
     const index = stops.findIndex(s => s.id === stop.id)
-    if(index >= 0){
-        if(stops[index].name === stop.name)
+    if (index >= 0) {
+        if (stops[index].name === stop.name)
             return AddStopEffect.None
 
         stops[index].name = stop.name
+        await storeStops()
         return AddStopEffect.Updated
     }
     stops.push(stop)
+    await storeStops()
     return AddStopEffect.Added
 }
 
-// deno-lint-ignore require-await
-export async function getStops(): Promise<Stop[]>{
+
+let initialized = false
+export async function getStops(): Promise<Stop[]> {
+    if (initialized)
+        return stops
+    await restoreStops()
+    initialized = true
     return stops
+}
+
+async function storeStops() {
+    await Deno.writeTextFile(stopsFileName, JSON.stringify(stops, null, 4))
+}
+
+async function restoreStops() {
+    try {
+        const json = await Deno.readTextFile(stopsFileName)
+        stops = JSON.parse(json)
+        // deno-lint-ignore no-empty
+    } catch { }
 }
