edafd3d543fc35d294d83e2051142ab9927c6123
Kirill Polferov <kirill.polferov@appfact.de>
2023-02-08 18:04:48 +0100

    basic info screen impl

---
+++ frontend/src/components/info.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { Info } from "../../../types";
+  export let info: Info;
+</script>
+
+<div class="info">
+  <ul>
+    {#each info.actual as infoItem}
+      <li class="item">
+        <span>{infoItem.patternText} -> {infoItem.direction}</span>
+        <span>
+          {#if infoItem.actualTime !== infoItem.plannedTime}
+            <span class="planned">
+              {infoItem.plannedTime}
+            </span>
+          {/if}
+          <span>{infoItem.actualTime}</span>
+        </span>
+        <span>{infoItem.actualRelativeTime}</span>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .info ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  .item {
+    margin-bottom: 1rem;
+  }
+
+  .item > span {
+    display: block;
+  }
+
+  .planned {
+    text-decoration: line-through;
+    margin-right: 0.25rem;
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import type { Info as InfoType } from "../../../types";
+  import Info from "../components/info.svelte";
   import Search from "../components/search.svelte";
 
+  let info = JSON.parse(localStorage.getItem("data") || "") as InfoType;
 </script>
 
-<Search/>
+<Search />
+<Info {info} />
