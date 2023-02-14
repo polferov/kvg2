238785232a4e2707808627c870c062564803a0ae
kirill <polferov@live.com>
2023-02-14 16:08:42 +0100

    extract info item into own component

--- frontend/src/components/info.svelte
+++ frontend/src/components/info.svelte
@@ -1,43 +1,20 @@
 <script lang="ts">
   import type { Info } from "../../../types";
+  import InfoItem from "./infoItem.svelte";
   export let info: Info;
 </script>
 
-<div class="info">
+<div>
   <ul>
     {#each info.actual as infoItem}
-      <li class="item">
-        <span>{infoItem.patternText} -> {infoItem.direction}</span>
-        <span>
-          {#if infoItem.actualTime !== infoItem.plannedTime && infoItem.plannedTime !== undefined}
-            <span class="planned">
-              {infoItem.plannedTime}
-            </span>
-          {/if}
-          <span>{infoItem.actualTime || infoItem.plannedTime}</span>
-        </span>
-        <span>{infoItem.actualRelativeTime}</span>
-      </li>
+      <InfoItem item={infoItem} />
     {/each}
   </ul>
 </div>
 
 <style>
-  .info ul {
+  ul {
     list-style: none;
     padding-left: 0;
-  }
-
-  .item {
-    margin-bottom: 1rem;
-  }
-
-  .item > span {
-    display: block;
-  }
-
-  .planned {
-    text-decoration: line-through;
-    margin-right: 0.25rem;
   }
 </style>
---
+++ frontend/src/components/infoItem.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { InfoItem as InfoItemType } from "../../../types";
+  export let item: InfoItemType;
+</script>
+
+<li class="item">
+  <span>{item.patternText} -> {item.direction}</span>
+  <span>
+    {#if item.actualTime !== item.plannedTime && item.plannedTime !== undefined}
+      <span class="planned">
+        {item.plannedTime}
+      </span>
+    {/if}
+    <span>{item.actualTime || item.plannedTime}</span>
+  </span>
+  <span>{item.actualRelativeTime}</span>
+</li>
+
+<style>
+  li {
+    margin-bottom: 1rem;
+  }
+
+  li > span {
+    display: block;
+  }
+
+  .planned {
+    text-decoration: line-through;
+    margin-right: 0.25rem;
+  }
+</style>
